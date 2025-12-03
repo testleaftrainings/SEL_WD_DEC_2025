54a5134b5e5b61845a4cd7f55a70060c50dfcc5c
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2025-12-03 15:27:08 +0530

    Add files via upload

---
+++ Week1/Day1/FirstProgram.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+public class FirstProgram {
+
+	//main - ctrl+spacebar
+	public static void main(String[] args) {
+		
+		//syso - ctrl+spacebar
+		System.out.println("Welcome to Testleaf");
+		
+	}
+}
---
+++ Week1/Day1/LearnDataTypes.java
@@ -0,0 +1,27 @@
+package week1.day1;
+
+public class LearnDataTypes {
+
+	public static void main(String[] args) {
+		
+	//DataTypes - Primitive, Non-Primitive
+		//Primitive - int, char, boolean, float.
+		//Non-Primitive - String, Array.
+		
+		//syntax for storing a value
+		//Datatype var.name = value;
+		
+		int month = 12;
+		String monthName ="DECEMBER";
+		boolean currentMonth = true;
+		char day = 'W';
+		float decimals = 1.090807f;
+		
+		System.out.println(month);
+		System.out.println(monthName);
+		System.out.println(currentMonth+" I Have given space "+day);
+		System.out.println(day);
+		System.out.println(decimals);
+	}
+
+}
